9ad1af3560a82c98871098413aacab40e437df54
akshaydobariya <avpatel7775@gmail.com>
2024-03-14 16:50:41 +0530

    music

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className=" max-h-screen bg-black/[96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+    </main>
+  );
+}
